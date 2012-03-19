6438d2cda30eca0244949cdf6610c6e8026593fc
akubaisi <asma@cmu.edu>
2012-03-19 23:13:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsmaProject3::Application.config.secret_token = '181245737733c46b1a1b86feb4c15bb4a75bc9c6d1841623994d46d79d269bd695e9efa911150049627249469052e677d002af9ded38a9949c3c45cc67e73c2a'
